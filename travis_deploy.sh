ae62b1e3af944a1a13951b561f9a6c22d4d95b8b
Benjamin Poldrack <benjaminpoldrack@gmail.com>
2018-02-23 08:14:00 +0100

    Add deploy script for auto-publishing the site

---
+++ travis_deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# simple script to use travis-encrypted github token and git user for updating docs folder whenever master branch was updated.
+# It assumes that make was done before and therefore docs is up-to-date locally.
+
+set -x
+git --version
+
+# set up git and github access
+git config user.name $GIT_NAME
+git config user.email $GIT_EMAIL
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:x-oauth-basic@github.com" > .git/credentials
+
+# switch to branch gh-pages and get the fresh build
+git remote set-branches --add origin gh-pages
+git fetch origin
+git branch -a
+git checkout origin/gh-pages
+rsync -r docs/ .
+
+# commit new docs folder and push
+git add .
+git commit -m "Automatically updated github page"
+git push origin HEAD:gh-pages
+
+rm .git/credentials
+
+
