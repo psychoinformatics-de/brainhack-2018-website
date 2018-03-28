3d6bcb2fc4b1ef70fe3fdae603130c841c2aedf2
Alex Waite <alex@waite.eu>
2018-03-28 11:25:19 +0200

    add jinja2content plugin; REQUIRES python3
    With this commit, we're bumping our reqs up to python3. Travis fixes to come.

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,7 +17,7 @@ LOCALE = u'en_US.UTF-8'
 # Configure Pelican a bit
 #
 PLUGIN_PATHS = ['pelican-plugins']
-PLUGINS = ['sitemap']
+PLUGINS = ['sitemap', 'jinja2content']
 SITEMAP = { 'format': 'xml' }
 
 THEME = 'theme'
